2839e196031580ca6a79aafd1d4b75651a16e203
Marcelo Bini <marcelobini@gmail.com>
2020-03-28 17:12:10 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,4 +8,9 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  index = 0
+  while index < directors_database[0][:movies].length do
+    puts directors_database[0][:movies][index][:title]
+    index += 1
+  end
 end
